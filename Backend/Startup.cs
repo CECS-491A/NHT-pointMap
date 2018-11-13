ea6f2238f00c1f1864468946b16e342e0264491d
Julian Poyourow <julianpoyo@gmail.com>
2018-11-12 20:15:38 -0800

    Added example PasswordService with hashing

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -10,8 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PointMap.Services;
 
-namespace Backend
+namespace PointMap
 {
     public class Startup
     {
@@ -26,6 +27,8 @@ namespace Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IPasswordService, PasswordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
