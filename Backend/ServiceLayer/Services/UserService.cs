02dfdc8d65b0bf68ada686fbf7adf2179fe757e9
Julian Poyourow <julianpoyo@gmail.com>
2018-12-01 07:56:59 -0800

    Moved "UserManagementManager" to services solution

---
+++ Backend/ServiceLayer/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.Services
+{
+    public class UserService : IUserService
+    {
+        public void Create()
+        {
+            
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void ToggleStatus()
+        {
+
+        }
+    }
+}
