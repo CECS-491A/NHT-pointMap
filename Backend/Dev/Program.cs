d5dc41a51431085f21a801d37edd45d1431984ce
Alfredo Vargas <vargas.alfredojr@gmail.com>
2018-12-02 11:13:59 -0800

    cleared up dev for testing

--- Backend/Dev/Program.cs
+++ Backend/Dev/Program.cs
@@ -1,11 +1,4 @@
-﻿using DataAccessLayer.Data;
-using DataAccessLayer.Models;
-using ServiceLayer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Program
 {
@@ -13,36 +6,9 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            IPasswordService _passwordService = new PasswordService();
-            Guid guid = new Guid(_passwordService.GenerateSalt());
-            Console.WriteLine(guid);
 
-            using (var _db = new DatabaseContext())
-            {
-                DateTime timestamp = DateTime.UtcNow;
-                DateTime dob = DateTime.Now.Date;
-                string userPassword = "mypasswordisnowhashedandhidden";
-                byte[] salt = _passwordService.GenerateSalt();
-                string hash = _passwordService.HashPassword(userPassword, salt);
-                var u = new User
-                {
-                    Id = guid,
-                    Email = "sauce@mail.com",
-                    UpdatedAt = timestamp,
-                    PasswordHash = hash,
-                    PasswordSalt = salt,
-                    DateOfBirth = dob
-                };
-                _db.Users.Add(u);
-                _db.SaveChanges();
-                var users = _db.Users;
-
-                foreach (var user in users.ToList())
-                {
-                    Console.WriteLine(user.Email, " ", user.PasswordHash.Length);
-                }
-                Console.ReadKey();
-            }
+            // create user
+            Console.ReadKey();
         }
     }
 }
