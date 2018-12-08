776af4f931a1ee3c156a8378f88f9573d5d373d7
ChrisMeyer7088 <chris_meyer_2@hotmail.com>
2018-12-07 20:36:31 -0800

    Added Operation/Service creation class and disabling class

---
+++ Backend/DataAccessLayer/Repositories/OperationRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+using System.Data.Entity;
+using DataAccessLayer.Database;
+
+namespace DataAccessLayer.Repositories
+{
+    public class OperationRepository
+    {
+
+        public int createService(Service service, DatabaseContext _db)
+        {
+            try
+            {
+                _db.Services.Add(service);
+                return 1;
+
+            } 
+            catch(Exception)
+            {
+                return 0;
+            }
+        }
+
+        public int createService(string serviceName, DatabaseContext _db)
+        {
+            try
+            {
+                Service service = new Service
+                {
+                    ServiceName = serviceName,
+                    Disabled = false
+                };
+
+                _db.Services.Add(service);
+                return 1;
+
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public int disableService(Guid id, DatabaseContext _db)
+        {
+            try
+            {
+                Service service = _db.Services.Find(id);
+                //service.Disabled = true;
+                //service.UpdatedAt = DateTime.UtcNow;
+                //_db.Entry(service).State = EntityState.Modified;
+                return 1;
+            }
+            catch(Exception)
+            {
+                return 0;
+            }
+        }
+
+        public int enableService(Guid id, DatabaseContext _db)
+        {
+            try
+            {
+                Service service = _db.Services
+                    .Where(s => s.Id == id)
+                    .FirstOrDefault();
+                service.Disabled = false;
+                service.UpdatedAt = DateTime.UtcNow;
+                _db.Entry(service).State = EntityState.Modified;
+                return 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Backend/UnitTesting/OperationServiceUT.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLayer.Services;
+using DataAccessLayer.Models;
+using DataAccessLayer.Database;
+
+namespace UnitTesting
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class OperationServiceUT
+    {
+        OperationService _os;
+        Service service1;
+        Service service2;
+        TestingUtils _ts;
+
+        public OperationServiceUT()
+        {
+            _os = new OperationService();
+            _ts = new TestingUtils();
+        }
+
+        [TestMethod]
+        public void createServiceObject()
+        {
+            service1 = new Service
+            {
+                ServiceName = "" + Guid.NewGuid(),
+                Disabled = false
+                
+            };
+            using (var _db = new DatabaseContext())
+            {
+                Assert.AreEqual(1, _os.createService(service1, _db));
+                Assert.AreEqual(0, _os.createService(service2, _db));
+            }
+        }
+
+        [TestMethod]
+        public void createServiceString()
+        {
+            string serviceName = "" + Guid.NewGuid();
+            using (var _db = new DatabaseContext())
+            {
+                Assert.AreEqual(1, _os.createService(serviceName, _db));
+            }
+        }
+
+        [TestMethod]
+        public void disableService()
+        {
+            service1 = _ts.CreateService(true);
+            service2 = _ts.CreateService(true);
+
+            using (var _db = new DatabaseContext())
+            {
+                Assert.AreEqual(1, _os.disableService(service1.Id, _db));
+                Assert.AreEqual(1, _os.disableService(service2.Id, _db));
+            }
+        }
+
+        [TestMethod]
+        public void enableService()
+        {
+            service1 = _ts.CreateService(false);
+            service2 = _ts.CreateService(false);
+
+            using (var _db = new DatabaseContext())
+            {
+                Assert.AreEqual(1, _os.enableService(service1.Id, _db));
+                Assert.AreEqual(1, _os.enableService(service2.Id, _db));
+            }
+        }
+    }
+}
