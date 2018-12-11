165791fdce125b5b5ba501388f1507cf3ba47a5d
Ryan Cole <ryantcole@gmail.com>
2018-12-11 02:05:59 -0800

    adding signitures for client service interface

--- Backend/ServiceLayer/Services/IClientService.cs
+++ Backend/ServiceLayer/Services/IClientService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Database;
+using DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,9 @@ namespace ServiceLayer.Services
 {
     public interface IClientService
     {
-
+        Client CreateClient(DatabaseContext _db, Client client);
+        Client DeleteClient(DatabaseContext _db, Guid Id);
+        Client GetClient(DatabaseContext _db, Guid Id);
+        Client UpdateClient(DatabaseContext _db, Client client);
     }
 }
