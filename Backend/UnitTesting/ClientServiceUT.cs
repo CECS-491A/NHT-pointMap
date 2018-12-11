e25fee53465728f7f2a03c6d6c4bb73acde98990
Ryan Cole <ryantcole@gmail.com>
2018-12-11 00:30:07 -0800

    oeu

---
+++ Backend/UnitTesting/ClientServiceUT.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLayer.Services;
+using DataAccessLayer.Models;
+using DataAccessLayer.Database;
+
+namespace UnitTesting
+{
+   
+    [TestClass]
+    public class ClientServiceUT
+    {
+        Client client;
+        ClientService cs;
+        DatabaseContext _db;
+
+        public ClientServiceUT()
+        {
+           
+        }
+        [TestMethod]
+        public Client CreateClient()
+        {
+            
+        }
+        [TestMethod]
+        public Client DeleteClient()
+        {
+        }
+        [TestMethod]
+        public Client GetClient()
+        {
+            
+        }
+        [TestMethod]
+        public Client UpdateClient()
+        {
+            
+        }
+
+    }
+}
