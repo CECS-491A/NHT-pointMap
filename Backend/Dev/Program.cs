b853c72eb980b054cc2155b95020cf508a721aa9
Julian Poyourow <julianpoyo@gmail.com>
2018-12-11 16:06:56 -0800

    Removed changes to program.cs

--- Backend/Dev/Program.cs
+++ Backend/Dev/Program.cs
@@ -13,10 +13,10 @@ namespace Program
             // the business req's to crete a user should go in the manger
             //  - call services to create user and add to the database
 
-            // UserManagementManager umm = new UserManagementManager();
-            // Guid guid = new Guid("f8d0c634-159e-4e8a-a561-19bc118a1b49");
-            // UserManager userManger = new UserManager();
-            // umm.DeleteUser(guid);
+//            UserManagementManager umm = new UserManagementManager();
+  //          Guid guid = new Guid("f8d0c634-159e-4e8a-a561-19bc118a1b49");
+    //        UserManager userManger = new UserManager();
+      //      umm.DeleteUser(guid);
             Console.ReadKey();
         }
     }
