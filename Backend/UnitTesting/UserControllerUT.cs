3d5d646e1613ab6d84117765e8f7b1b38700559a
Alfredo Vargas <vargas.alfredojr@gmail.com>
2019-02-22 23:24:22 -0800

    working on ut for user controller

---
+++ Backend/UnitTesting/UserControllerUT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApi_PointMap.Controllers;
+using WebApi_PointMap.Models;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UserControllerUT
+    {
+        [TestMethod]
+        public void GetUser()
+        {
+            // Arrange
+            var controller = new UserController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // Act
+            var response = controller.Get(10);
+
+            // Assert
+            var expectedResult = "alfredo";
+            var result = controller.Get() as OkNegotiatedContentResult<string>;
+            //Assert.AreEqual(expectedResult, result.Content.);
+        }
+    }
+}
