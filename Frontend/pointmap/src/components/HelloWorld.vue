5df35ef1056dda9b29df1a0b699f306f2c2537b2
ChrisMeyer7088 <chris_meyer_2@hotmail.com>
2019-02-24 19:23:05 -0800

    Added vuejs helloworld

---
+++ Frontend/pointmap/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class='HelloWorld'>
+    <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:58896/api/user')
+      .then(response => (this.msg = response))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+ 
