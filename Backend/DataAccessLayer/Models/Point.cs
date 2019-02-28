4536cc10e4e14e4766af29a33c0d1bde040fa3d3
Luke Clements <lukecjm@icloud.com>
2019-02-27 22:35:09 -0800

    begin work on point backend

---
+++ Backend/DataAccessLayer/Models/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Models
+{
+    public class Point
+    {
+        public Point()
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public float Longitude { get; set; }
+        [Required]
+        public float Latitude { get; set; }
+
+        [Required, Column(TypeName = "datetime2"), DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+
+        [Required, Column(TypeName = "datetime2"), DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Backend/DataAccessLayer/Repositories/PointRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Database;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class PointRepository
+    {
+        public int CreatePoint(Point point)
+        {
+            using (var _db = new DatabaseContext())
+            {
+                point.UpdatedAt = DateTime.UtcNow;
+                try
+                {
+                    _db.Points.Add(point);
+                    return _db.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public Service GetService(string pointName)
+        {
+            using (var _db = new DatabaseContext())
+            {
+                Service service = _db.Services
+                    .Where(c => c.ServiceName == pointName)
+                    .FirstOrDefault();
+                return service;
+            }
+        }
+    }
+}
