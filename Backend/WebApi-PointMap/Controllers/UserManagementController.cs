17d805417988e720b4911832e2c3d38bd57efa50
Alfredo Vargas <vargas.alfredojr@gmail.com>
2019-03-28 03:36:31 -0700

    updated management controller

--- Backend/WebApi-PointMap/Controllers/UserManagementController.cs
+++ Backend/WebApi-PointMap/Controllers/UserManagementController.cs
@@ -1,10 +1,14 @@
 ﻿using DataAccessLayer.Database;
+using ManagerLayer.UserManagement;
+using ServiceLayer.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using static ServiceLayer.Services.ExceptionService;
 
 namespace WebApi_PointMap.Controllers
 {
@@ -19,11 +23,15 @@ namespace WebApi_PointMap.Controllers
                 try
                 {
                     var users = _db.Users
-                    .Select(p => new
+                    .Select(u => new
                     {
-                        id = p.Id,
-                        username = p.Username,
-                        manager = p.ManagerId
+                        id = u.Id,
+                        username = u.Username,
+                        manager = u.ManagerId,
+                        city = u.City,
+                        state = u.State,
+                        countty = u.Country,
+                        disabled = u.Disabled
                     }).ToList();
                     return Ok(users);
                 }
@@ -60,7 +68,11 @@ namespace WebApi_PointMap.Controllers
                         .Select(u => new {
                             id = u.Id,
                             username = u.Username,
-                            manager = u.ManagerId
+                            manager = u.ManagerId,
+                            city = u.City,
+                            state = u.State,
+                            country = u.Country,
+                            disabled = u.Disabled
                         }).ToList();
                     return Content((HttpStatusCode)200, users);
                 }
@@ -69,6 +81,102 @@ namespace WebApi_PointMap.Controllers
                     return InternalServerError();
                 }
             }
+        }
+
+        [HttpDelete]
+        [Route("user/delete/{userId}")]
+        public IHttpActionResult DeleteUser(string userId)
+        {
+            if (userId == null)
+            {
+                return Content((HttpStatusCode)412, "Invalid payload.");
+            }
+            Guid UserId;
+            try
+            {
+                // check if valid SSO ID format
+                UserId = Guid.Parse(userId);
+            }
+            catch (Exception)
+            {
+                return Content((HttpStatusCode)400, "Invalid User ID");
+            }
+            using(var _db = new DatabaseContext())
+            {
+                UserManagementManager _userManager = new UserManagementManager(_db);
+                try
+                {
+                    _userManager.DeleteUser(UserId);
+                    _db.SaveChanges();
+                    return Ok("User was deleted");
+                }
+                catch (Exception ex)
+                {
+                    return Content((HttpStatusCode)500, ex.Message);
+                }
+            }
+        }
+
+        [HttpPut]
+        [Route("user/update")]
+        public IHttpActionResult UpdateUser([FromBody] UpdateUserRequestDTO payload)
+        {
+            if (!ModelState.IsValid || payload == null)
+            {
+                return Content((HttpStatusCode)412, ModelState);
+            }
+            Guid UserId;
+            try
+            {
+                // check if valid SSO ID format
+                UserId = Guid.Parse(payload.UserId);
+            }
+            catch (Exception)
+            {
+                return Content((HttpStatusCode)400, "Invalid User ID");
+            }
+            using (var _db = new DatabaseContext())
+            {
+                UserManagementManager _userManager = new UserManagementManager(_db);
+                var user = _userManager.GetUser(UserId);
+                if (user == null)
+                {
+                    return Content(HttpStatusCode.NotFound, "User does not exist.");
+                }
+                user.City = payload.City;
+                user.State = payload.State;
+                user.Country = payload.Country;
+                user.Disabled = payload.Disabled;
+                user.ManagerId = payload.ManagerId;
+                try
+                {
+                    _userManager.UpdateUser(user);
+                    _db.SaveChanges();
+                    return Content(HttpStatusCode.OK, "User updated");
+                }
+                catch (Exception)
+                {
+                    _db.Entry(user).CurrentValues.SetValues(_db.Entry(user).OriginalValues);
+                    _db.Entry(user).State = System.Data.Entity.EntityState.Unchanged;
+                    return Content(HttpStatusCode.InternalServerError, "User was not updated");
+                }
+            }
+        }
+
+        public class UpdateUserRequestDTO
+        {
+            [Required]
+            public string UserId { get; set; }
+            [Required]
+            public string City { get; set; }
+            [Required]
+            public string State { get; set; }
+            [Required]
+            public string Country { get; set; }
+            [Required]
+            public Guid ManagerId { get; set; }
+            [Required]
+            public bool Disabled { get; set; }
         }
 
     }
