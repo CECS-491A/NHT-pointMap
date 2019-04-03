33374dc8538c914767c1744204722b25ffb34851
Alfredo Vargas <vargas.alfredojr@gmail.com>
2019-04-02 23:02:05 -0700

    added alerts and dialogs for ui

--- Frontend/src/components/UserManagement.vue
+++ Frontend/src/components/UserManagement.vue
@@ -10,7 +10,6 @@
       <v-spacer></v-spacer>
       <v-dialog v-model="dialog" max-width="500px">
         <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark class="mb-2" v-on="on">New User</v-btn>
         </template>
         <v-card>
           <v-card-title>
@@ -81,49 +80,83 @@
         <v-btn color="primary" @click="initialize">Reset</v-btn>
       </template>
     </v-data-table>
+    <div v-if="loading">
+      <Loading :dialog="loading" :text="loadingText"/>
+    </div>
+    <div>
+      <v-snackbar
+        v-model="alertDialog.alert"
+        :color="alertDialog.alertType"
+        :top="alertDialog.top"
+      >
+        {{ alertDialog.alertText }}
+      <v-btn
+        dark
+        flat
+        @click="alertDialog.alert=false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+    </div>
   </div>
 </template>
 
 <script>
 import { GetUsers, UpdateUser, DeleteUser } from '@/services/userManagementServices';
+import Loading from '@/components/dialogs/Loading.vue'
+import AlertDialog from '@/components/dialogs/AlertDialog.vue'
+
   export default {
-      name: 'UserManagement',
-      data: () => ({
-        dialog: false,
-        headers: [
-          {
-            text: 'ID',
-            align: 'left',
-            sortable: false,
-            value: 'id'
-          },
-          { text: 'Username', value: 'username' },
-          { text: 'Manager', value: 'manager' },
-          { text: 'Disabled', value: 'disabled' },
-          { text: 'City', value: 'city' },
-          { text: 'State', value: 'state'},
-          { text: 'Country', value: 'country'},
-          { text: 'Actions', value: 'name', sortable: 'false'}
-        ],
-        users: [],
-        editedIndex: -1,
-        editedItem: {
-          id: '',
-          city: '',
-          state: '',
-          country: '',
-          managerId: '',
-          disabled: false
+    name: 'UserManagement',
+    components: {
+      Loading,
+      AlertDialog
+    },
+    data: () => ({
+      loading: false,
+      loadingText: '',
+      alertDialog: {
+        alert: false,
+        alertText: '',
+        alertType: '',
+        top: true
+      },
+      dialog: false,
+      headers: [
+        {
+          text: 'ID',
+          align: 'left',
+          sortable: false,
+          value: 'id'
         },
-        defaultItem: {
-          userId: '',
-          city: '',
-          state: '',
-          country: '',
-          managerId: '',
-          disabled: false
-        },
-        StatusOfData: 'No Data'
+        { text: 'Username', value: 'username' },
+        { text: 'Manager', value: 'manager' },
+        { text: 'Disabled', value: 'disabled' },
+        { text: 'City', value: 'city' },
+        { text: 'State', value: 'state'},
+        { text: 'Country', value: 'country'},
+        { text: 'Actions', value: 'name', sortable: 'false'}
+      ],
+      users: [],
+      editedIndex: -1,
+      editedItem: {
+        id: '',
+        city: '',
+        state: '',
+        country: '',
+        managerId: '',
+        disabled: false
+      },
+      defaultItem: {
+        userId: '',
+        city: '',
+        state: '',
+        country: '',
+        managerId: '',
+        disabled: false
+      },
+      StatusOfData: 'No Data'
     }),
 
     computed: {
@@ -140,19 +173,27 @@ import { GetUsers, UpdateUser, DeleteUser } from '@/services/userManagementServi
 
     created () {
       this.initialize();
-      console.log("Rendered");
     },
 
     methods: {
       initialize () {
         this.users = []
         this.StatusOfData = 'Loading Data...'
+        this.loading = true
+        this.loadingText = 'Loading Data...'
         GetUsers().then(
           response => {
             this.users = response;
           }
         )
-        .catch(this.StatusOfData = 'No Data Found.')
+        .catch(err => {
+          this.StatusOfData = 'No Data Found.'
+
+        })
+        .finally(() => {
+          this.loading = false
+          this.loadingText = ''
+        })
       },
 
       editItem (item) {
@@ -164,11 +205,22 @@ import { GetUsers, UpdateUser, DeleteUser } from '@/services/userManagementServi
       deleteItem (item) {
         const index = this.users.indexOf(item)
         if (confirm('Are you sure you want to delete this item?') && this.users.splice(index, 1)){
+          this.loading = true
+          this.loadingText = 'Deleting User...'
           DeleteUser(item.id)
-            .then(console.log(response))
+            .then(response => {
+              this.alertDialog.alert = true
+              this.alertDialog.alertText = 'User was deleted'
+              this.alertDialog.alertType = 'success'
+            })
             .catch(err => {
-              console.log(err.response.status);
-            });
+              this.alertDialog.alert = true
+              this.alertDialog.alertText = 'User was not deleted'
+              this.alertDialog.alertType = 'error'
+            })
+            .finally(() => {
+              this.loading = false
+            })
         }
       },
 
@@ -182,13 +234,27 @@ import { GetUsers, UpdateUser, DeleteUser } from '@/services/userManagementServi
 
       save () {
         if (this.editedIndex > -1) {
+          this.loading = true
+          this.loadingText = 'Updating User...'
           UpdateUser(this.editedItem)
+            .then(reponse => {
+              this.alertDialog.alert = true
+              this.alertDialog.alertText = 'User was updated'
+              this.alertDialog.alertType = 'success'
+            })
+            .catch(err => {
+              this.alertDialog.alert = true
+              this.alertDialog.alertText = 'User was not updated'
+              this.AlertDialog.alertType = 'error'
+            })
+            .finally(() => {
+              this.loading = false
+              this.initialize()
+            })
           Object.assign(this.users[this.editedIndex], this.editedItem)
         } else {
           this.users.push(this.editedItem)
         }
-        console.log('updated')
-        this.initialize()
         this.close()
       }
     }
