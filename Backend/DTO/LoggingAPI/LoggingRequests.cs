3e1e837105936a2b3ff01455bdf4f8f7b05f5db3
Alfredo Vargas <vargas.alfredojr@gmail.com>
2019-05-10 21:10:09 -0700

    started webpage logging

---
+++ Backend/DTO/LoggingAPI/LoggingRequests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.LoggingAPI
+{
+    public class LogWebpageUsageRequest
+    {
+        [Required]
+        public string Page { get; set; }
+
+        [Required]
+        public long DurationStart { get; set; }
+
+        [Required]
+        public long DurationEnd { get; set; }
+
+        // Passed in as a Request Header [token]
+        public string Token { get; set; }
+
+        // TODO: Make required
+        public string Signature { get; set; }
+
+        // TODO: Make Required
+        public string Salt { get; set; }
+
+        public long Duration { get; set; }
+    }
+
+}
---
+++ Backend/WebApi-PointMap/Controllers/LoggingController.cs
@@ -0,0 +1,62 @@
+﻿using DTO.Constants;
+using DTO.LoggingAPI;
+using System;
+using System.Net;
+using System.Web.Http;
+using static ServiceLayer.Services.ExceptionService;
+
+namespace WebApi_PointMap.Controllers
+{
+    /// <summary>
+    /// Logging Enpoints:
+    ///     - Admin Dash Analytics
+    ///         - The top 5 most used application features
+    ///         - The top 5 most time spent on app webpage
+    /// </summary>
+    public class LoggingController : ApiController
+    {
+        // Token is passed in header of request
+        [Route("api/log/webpageusage")]
+        [HttpPost]
+        public IHttpActionResult LogWebpageUsage(LogWebpageUsageRequest payload)
+        {
+            try
+            {
+                // Throws SessionNotFound
+                var session = ControllerHelpers.ValidateSession(Request);
+                Enum.IsDefined(typeof(Constants.Pages), payload.Page);
+
+                return Ok();
+            }
+            catch (Exception e) when (e is NoTokenProvidedException ||
+                                            e is SessionNotFoundException)
+            {
+                return Content(HttpStatusCode.Unauthorized, e.Message);
+            }
+            catch (Exception e) when (e is ArgumentException)
+            {
+                return Content(HttpStatusCode.PreconditionFailed, e.Message);
+            }
+        }
+
+        // Token is passed in header of request
+        [Route("api/log/appfeatureusage")]
+        [HttpPost]
+        public IHttpActionResult LogAppFeatureUsage()
+        {
+
+            try
+            {
+                // Throws SessionNotFound
+                var session = ControllerHelpers.ValidateSession(Request);
+
+                return Ok();
+            }
+            catch (Exception e) when (e is NoTokenProvidedException ||
+                                            e is SessionNotFoundException)
+            {
+                return Content(HttpStatusCode.Unauthorized, e.Message);
+            }
+        }
+    }
+}
